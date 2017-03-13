43e9ecd410a25135578e49021fb7a53f7953603c
npapatonis <npapatonis@users.noreply.github.com>
2017-03-13 14:06:06 -0400

    Create GetFileSizes.sql

---
+++ File Management/GetFileSizes.sql
@@ -0,0 +1,1 @@
+SELECT [name], CONVERT(DEC(19, 2), [size]) * 8192 / 1048576.0 AS [size_mb] FROM sys.master_files WHERE [database_id] = DB_ID('Guard1Track') OR [database_id] = DB_ID('Guard1TrackWarehouse')
